6ba00fb45f47f0947adcd44f262d5af4fa70a1bf
abanoubalameer0109 <abanoub.ayman.0109@gmail.com>
2026-07-24 12:33:39 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -30,201 +30,157 @@ typedef unsigned short u16;
 #define HIGH     1
 #define LOW      0
 
-#define NOT_PRESSED 0xFF
+void SetPinDirection(u8 Port, u8 pin_no, u8 value);
+void SetPinValue(u8 Port, u8 pin_no, u8 value);
+u8   GetPinValue(u8 Port, u8 pin_no);
+void TogglePinValue(u8 Port, u8 pin_no);
 
-/* Prototypes */
-void DIO_SetPinDirection(u8 Port, u8 pin_no, u8 value);
-void DIO_SetPinValue(u8 Port, u8 pin_no, u8 value);
-u8   DIO_GetPinValue(u8 Port, u8 pin_no);
-void DIO_TogglePinValue(u8 Port, u8 pin_no);
+void SetPortDirection(u8 port, u8 PortDir);
+void SetPortValue(u8 port, u8 value);
+void TogglePortValue(u8 port);
+u8   GetPortValue(u8 port);
 
-void DIO_SetPortDirection(u8 port, u8 PortDir);
-void DIO_SetPortValue(u8 port, u8 value);
-void DIO_TogglePortValue(u8 port);
-u8   DIO_GetPortValue(u8 port);
 
 void lcd_send_command(u8 command);
 void lcd_send_data(u8 data);
 void lcd_send_string(u8* str);
 void lcd_init(void);
 
-void KEYPAD_Init(void);
+void KEYPAD_Init(void) ;
 u8 PressedKey(void);
-
-void NumberToString(u8 num, u8* str);
-
-const u8 Keypad_Buttons[4][4] = {
-	{'7', '8', '9', '/'},
-	{'4', '5', '6', '*'},
-	{'1', '2', '3', '-'},
-	{'C', '0', '=', '+'}
-};
-
+ u8 keys[4][4] =
+ {
+	 {'7', '8', '9', '/'},
+	 {'4', '5', '6', '*'},
+	 {'1', '2', '3', '-'},
+	 {'C', '0', '=', '+'}
+ }; 
+u8 result ();
+ u8 key;
 int main(void)
 {
-	u8 key = NOT_PRESSED;
-	u8 num1 = 0;
-	u8 num2 = 0;
-	u8 operation = 0;
-	u8 result = 0;
-	u8 result_string[10];
+	u8 res;
 
-	DIO_SetPortDirection(PORT_C, 0xFF);
-DIO_SetPortDirection(PORT_D, 0xFF);
 	lcd_init();
 	KEYPAD_Init();
 
 	while (1)
 	{
+		res = result();
+
+		lcd_send_data(res + '0');
+	}
+}
+u8 result ()
+{
+u8 n1,n2,or,res ;
+
+while (1)
+{
+	key = PressedKey();
+	if (key>= '0' && key <='9')
+	{
+		n1 =key-'0';
+	lcd_send_data(key);
+		_delay_ms(1000);
+		break;
+	}
+}
+	while (1)
+	{
 		key = PressedKey();
-
-		if (key != NOT_PRESSED)
+		if (key == '+' || key == '-' || key == '*' || key == '/')
 		{
-			if (key >= '0' && key <= '9')
-			{
+			or =key;
 				lcd_send_data(key);
-
-				if (operation == 0)
-				{
-					num1 = key - '0';
-				}
-				else
-				{
-					num2 = key - '0';
-				}
-			}
-
-			else if (key == '+' || key == '-' || key == '*' || key == '/')
-			{
-				operation = key;
-				lcd_send_data(key);
-			}
-
-			else if (key == '=')
-			{
-				lcd_send_data('=');
-
-				switch (operation)
-				{
-					case '+':
-					result = num1 + num2;
-					break;
-
-					case '-':
-					result = num1 - num2;
-					break;
-
-					case '*':
-					result = num1 * num2;
-					break;
-
-					case '/':
-					if (num2 != 0)
-					{
-						result = num1 / num2;
-					}
-					else
-					{
-						result = 0;
-					}
-					break;
-				}
-
-				NumberToString(result, result_string);
-				lcd_send_string(result_string);
-
-				num1 = 0;
-				num2 = 0;
-				operation = 0;
-			}
-
-			else if (key == 'C')
-			{
-				lcd_send_command(0x01);
-				_delay_ms(2);
-
-				num1 = 0;
-				num2 = 0;
-				result = 0;
-				operation = 0;
-			}
-		}
-	}
-}
-
-void NumberToString(u8 num, u8* str) {
-	u8 i = 0;
-	u8 temp[10];
-	u8 j = 0;
-
-	if (num == 0) {
-		str[0] = '0';
-		str[1] = '\0';
-		return;
-	}
-
-	while (num > 0) {
-		temp[i++] = (num % 10) + '0';
-		num /= 10;
-	}
-
-	while (i > 0) {
-		str[j++] = temp[--i];
-	}
-	str[j] = '\0';
-}
-
-void KEYPAD_Init(void) {
-	DIO_SetPinDirection(PORT_A, 0, OUTPUT);
-	DIO_SetPinDirection(PORT_A, 1, OUTPUT);
-	DIO_SetPinDirection(PORT_A, 2, OUTPUT);
-	DIO_SetPinDirection(PORT_A, 3, OUTPUT);
-
-	DIO_SetPinValue(PORT_A, 0, HIGH);
-	DIO_SetPinValue(PORT_A, 1, HIGH);
-	DIO_SetPinValue(PORT_A, 2, HIGH);
-	DIO_SetPinValue(PORT_A, 3, HIGH);
-
-	DIO_SetPinDirection(PORT_A, 4, INPUT);
-	DIO_SetPinDirection(PORT_A, 5, INPUT);
-	DIO_SetPinDirection(PORT_A, 6, INPUT);
-	DIO_SetPinDirection(PORT_A, 7, INPUT);
-
-	DIO_SetPinValue(PORT_A, 4, HIGH);
-	DIO_SetPinValue(PORT_A, 5, HIGH);
-	DIO_SetPinValue(PORT_A, 6, HIGH);
-	DIO_SetPinValue(PORT_A, 7, HIGH);
-}
-
-u8 PressedKey(void) {
-	u8 key = NOT_PRESSED;
-	u8 row_pins[4] = {0,1,2,3};
-	u8 col_pins[4] = {4, 5, 6, 7};
-
-	for (u8 r = 0; r < 4; r++) {
-		DIO_SetPinValue(PORT_A, row_pins[r], LOW);
-
-		for (u8 c = 0; c < 4; c++) {
-			if (DIO_GetPinValue(PORT_A, col_pins[c]) == LOW) {
-				_delay_ms(30);
-				while (DIO_GetPinValue(PORT_A, col_pins[c]) == LOW);
-				
-				key = Keypad_Buttons[r][c];
-				break;
-			}
-		}
-
-		DIO_SetPinValue(PORT_A, row_pins[r], HIGH);
-
-		if (key != NOT_PRESSED) {
 			break;
 		}
 	}
+			while (1)
+		{
+			key = PressedKey();
+			if (key>= '0' && key <='9')
+			{
+				n2 =key-'0';
+					lcd_send_data(key);
+				break;
+			}
+		}
+				while (1)
+				{
+					key = PressedKey();
+					if (key=='=')
+					{
+						lcd_send_data(key);
+					 if (or=='+')
+					{
+						return res =n1+n2;
+					}
+					else if (or=='-')
+					{
+						return res =n1-n2;
+					}
+					else if (or=='*')
+					{
+						return res =n1*n2;
+					}
+					else if (or=='/')
+					{
+						return res =n1/n2;
+					}
+					}
+				}
+	
+ }
 
-	return key;
+u8 PressedKey(void)
+{
+u8 row, col;
+
+for(row=0; row<4; row++)
+{
+	SetPinValue(PORT_A,0,HIGH);
+	SetPinValue(PORT_A,1,HIGH);
+	SetPinValue(PORT_A,2,HIGH);
+	SetPinValue(PORT_A,3,HIGH);
+
+	SetPinValue(PORT_A,row,LOW);
+
+	for( col=0; col<4; col++)
+	{
+		if(GetPinValue(PORT_A,col+4)==LOW)
+		{
+			_delay_ms(20);
+
+			while(GetPinValue(PORT_A,col+4)==LOW);
+
+			return keys[row][col];
+		}
+	}
+}
+return 0;
+}
+void KEYPAD_Init(void)
+{
+	SetPinDirection(PORT_A,0,OUTPUT);
+	SetPinDirection(PORT_A,1,OUTPUT);
+	SetPinDirection(PORT_A,2,OUTPUT);
+	SetPinDirection(PORT_A,3,OUTPUT);
+
+	SetPinDirection(PORT_A,4,INPUT);
+	SetPinDirection(PORT_A,5,INPUT);
+	SetPinDirection(PORT_A,6,INPUT);
+	SetPinDirection(PORT_A,7,INPUT);
+	
+	SetPortValue(PORT_A,0xFF);
 }
 
+
 void lcd_init(void) {
+
+	 SetPortDirection(PORT_C,0xFF);
+	 SetPortDirection(PORT_D,0xFF);
+	 
 	_delay_ms(50);
 
 	lcd_send_command(0x38);
@@ -243,25 +199,25 @@ void lcd_init(void) {
 }
 
 void lcd_send_command(u8 command) {
-	DIO_SetPinValue(PORT_C, 5, LOW);
-	DIO_SetPinValue(PORT_C, 4, LOW);
+	SetPinValue(PORT_C, 5, LOW);
+	SetPinValue(PORT_C, 4, LOW);
 
-	DIO_SetPortValue(PORT_D, command);
+	SetPortValue(PORT_D, command);
 
-	DIO_SetPinValue(PORT_C, 7, HIGH);
+	SetPinValue(PORT_C, 7, HIGH);
 	_delay_ms(2);
-	DIO_SetPinValue(PORT_C, 7, LOW);
+	SetPinValue(PORT_C, 7, LOW);
 	_delay_ms(5);
 }
 
 void lcd_send_data(u8 data) {
-	DIO_SetPinValue(PORT_C, 5, LOW);
-	DIO_SetPinValue(PORT_C, 4, HIGH);
+	SetPinValue(PORT_C, 5, LOW);
+	SetPinValue(PORT_C, 4, HIGH);
 
-	DIO_SetPortValue(PORT_D, data);
+	SetPortValue(PORT_D, data);
 
-	DIO_SetPinValue(PORT_C, 7, HIGH);
-	DIO_SetPinValue(PORT_C, 7, LOW);
+	SetPinValue(PORT_C, 7, HIGH);
+	SetPinValue(PORT_C, 7, LOW);
 	_delay_ms(5);
 }
 
@@ -273,7 +229,12 @@ void lcd_send_string(u8* str) {
 	}
 }
 
-void DIO_SetPinDirection(u8 Port, u8 pin_no, u8 value) {
+
+
+
+
+
+void SetPinDirection(u8 Port, u8 pin_no, u8 value) {
 	if (value == OUTPUT) {
 		switch (Port) {
 			case PORT_A: SET_BIT(DDRA, pin_no); break;
@@ -292,7 +253,7 @@ void DIO_SetPinDirection(u8 Port, u8 pin_no, u8 value) {
 	}
 }
 
-void DIO_SetPinValue(u8 Port, u8 pin_no, u8 value) {
+void SetPinValue(u8 Port, u8 pin_no, u8 value) {
 	if (value == HIGH) {
 		switch (Port) {
 			case PORT_A: SET_BIT(PORTA, pin_no); break;
@@ -311,7 +272,7 @@ void DIO_SetPinValue(u8 Port, u8 pin_no, u8 value) {
 	}
 }
 
-u8 DIO_GetPinValue(u8 Port, u8 pin_no) {
+u8 GetPinValue(u8 Port, u8 pin_no) {
 	u8 result = 0;
 	switch (Port) {
 		case PORT_A: result = (PINA >> pin_no) & 1; break;
@@ -322,7 +283,7 @@ u8 DIO_GetPinValue(u8 Port, u8 pin_no) {
 	return result;
 }
 
-void DIO_TogglePinValue(u8 Port, u8 pin_no) {
+void TogglePinValue(u8 Port, u8 pin_no) {
 	switch (Port) {
 		case PORT_A: TOG_BIT(PORTA, pin_no); break;
 		case PORT_B: TOG_BIT(PORTB, pin_no); break;
@@ -331,7 +292,7 @@ void DIO_TogglePinValue(u8 Port, u8 pin_no) {
 	}
 }
 
-void DIO_SetPortDirection(u8 port, u8 PortDir){
+void SetPortDirection(u8 port, u8 PortDir){
 	switch(port){
 		case PORT_A: DDRA = PortDir; break;
 		case PORT_B: DDRB = PortDir; break;
@@ -340,7 +301,7 @@ void DIO_SetPortDirection(u8 port, u8 PortDir){
 	}
 }
 
-void DIO_SetPortValue(u8 port, u8 value){
+void SetPortValue(u8 port, u8 value){
 	switch(port){
 		case PORT_A: PORTA = value; break;
 		case PORT_B: PORTB = value; break;
@@ -349,7 +310,7 @@ void DIO_SetPortValue(u8 port, u8 value){
 	}
 }
 
-void DIO_TogglePortValue(u8 port) {
+void TogglePortValue(u8 port) {
 	switch (port) {
 		case PORT_A: PORTA = ~PORTA; break;
 		case PORT_B: PORTB = ~PORTB; break;
@@ -358,7 +319,7 @@ void DIO_TogglePortValue(u8 port) {
 	}
 }
 
-u8 DIO_GetPortValue(u8 Port) {
+u8 GetPortValue(u8 Port) {
 	u8 result = 0;
 	switch (Port) {
 		case PORT_A: result = PINA; break;
@@ -368,3 +329,4 @@ u8 DIO_GetPortValue(u8 Port) {
 	}
 	return result;
 }
+
